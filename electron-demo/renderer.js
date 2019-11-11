7f365866a93c4a3dabc0a4ae1de19df226891479
yyccQQu <643929860@qq.com>
2019-11-11 09:20:49 +0700

    did it

---
+++ electron-demo/renderer.js
@@ -0,0 +1,17 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const { ipcRenderer } = require('electron')
+const { BrowserWindow } = require('electron').remote
+
+window.addEventListener('DOMContentLoaded', () => {  
+  document.getElementById('node-version').innerHTML = process.versions.node
+  document.getElementById('send').addEventListener('click', () => {
+    ipcRenderer.send('message', 'hello from renderer')
+    let win = new BrowserWindow({ width: 800, height: 600 })
+    win.loadURL('https://baidu.com')
+  })
+  ipcRenderer.on('reply', (event, arg) => {
+    document.getElementById('message').innerHTML = arg
+  })
+})
